f3ddf1b625f753b2d6660b7d2ea87850f4a99a66
Zhdzd <zahidabdullah18@gmail.com>
2022-01-24 14:16:41 +0800

    'commit'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'first_project';
+  title = 'Day 1 Workshop';
+  myNumber: number;
+    constructor(){
+      this.myNumber= 123;
+    }
+
 }
